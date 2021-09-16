6d283c44df752dba5e3cca9ca31ed539d396e4e0
vi3tnguyen <vi3t.nnq@gmail.com>
2021-09-16 10:42:19 +0700

    message

---
+++ src/main/java/Exercise_Week_1.java
@@ -0,0 +1,40 @@
+public class Exercise_Week_1 {
+
+    // Cộng 2 số trong Java
+    // Check 2 số chẵn lẽ
+
+    int a;
+    int b;
+
+
+    void checkValue(int a, int b){
+        this.a = a;
+        this.b = b;
+        int c = a+b;
+
+        int d = a * b;
+
+        System.out.println("Sum of value C: " + c);
+
+        if(a %2 == 0){
+            System.out.println("Giá trị chẳn " + a);
+        }else{
+            System.out.println("Giá trị lẻ " + a);
+        }
+
+
+        System.out.println("Nhân 2 số trong java " + d);
+    }
+
+
+
+    public static void main(String[] args) {
+        Exercise_Week_1 ex = new Exercise_Week_1();
+        ex.checkValue(2,4);
+
+
+
+    }
+
+
+}
