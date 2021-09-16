16faca00c3ffdf0d4eef61f6dcbc756878ce0221
vi3tnguyen <vi3t.nnq@gmail.com>
2021-09-16 22:33:47 +0700

    them mot dong

--- Beginner/src/main/java/Week_1/Exercise_Week_2.java
+++ Beginner/src/main/java/Week_1/Exercise_Week_2.java
@@ -6,6 +6,9 @@ public class Exercise_Week_2 {
     // Practice .....
 
     public static void main(String[] args) {
+        System.out.println("hahah");
         System.out.println(" HIHIHI");
     }
+
+    
 }
