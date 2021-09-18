088d565029a0e74ee3bf2da8911b1463a4354140
vi3tnguyen <vi3t.nnq@gmail.com>
2021-09-18 14:55:42 +0700

    Change

--- Beginner/src/main/java/Week_1/TEST.java
+++ Beginner/src/main/java/Week_1/TEST.java
@@ -4,5 +4,7 @@ public class TEST {
 
     public static void main(String[] args) {
         System.out.println("dasdasdsda");
+        System.out.println("......");
+        System.out.println(".....");
     }
 }
